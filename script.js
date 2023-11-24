ab7cb911207209a163cbb8112e68851b3d02ad17
Haniel Ulises <hanielulises2003@gmail.com>
2023-11-23 19:37:39 -0600

    Data structures class

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-class Queue{
-
-}
-
-class Stack{
-
+class Node{
+    constructor(element){
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+    }
 }
 
 class linkedList{
+    constructor(){
+        this.head = head;
+        this.tail = tail;
+    }
+
+    append(element){
+        const newNode = new Node (element);
+        if(!this.head){
+            this.head = newNode;
+        }else{
+            let current = this.head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+}
+
+// The rest of the data structures definitions will be based on the linked list
+
+class Queue{
+    constructor(){
+        this.first = first;
+        this.last = last;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first = first;
+        this.last = last;
+    }
 
 }
+
