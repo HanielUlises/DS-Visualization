3e71859bfb213256c6a5e4201b689d58adb30c8b
Haniel Ulises <hanielulises2003@gmail.com>
2023-11-24 18:44:59 -0600

    Data structures class

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ class linkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.size = 0;
     }
 
     append(element) {
@@ -26,6 +27,19 @@ class linkedList {
             newNode.prev = current;
             this.tail = newNode;
         }
+    }
+
+    retrieve(index) {
+        if (index < 0 || index >= this.size) {
+            return null; 
+        }
+    
+        let current = this.head;
+        for (let i = 0; i < index; i++) {
+            current = current.next;
+        }
+    
+        return current.element;
     }
 }
 
@@ -100,5 +114,60 @@ class Stack {
         }
         this.size--;
         return removedNode.element;
+    }
+}
+
+const linkedListInstance = new linkedList();
+const queueInstance = new Queue();
+const stackInstance = new Stack();
+
+function selectStructure() {
+    const selectedStructure = document.getElementById("dataStructureSelector").value;
+    document.getElementById("selectedStructureTitle").innerText = `Estructura seleccionada: ${selectedStructure}`;
+
+    updateOperationsSection(selectedStructure);
+}
+
+function updateOperationsSection(selectedStructure) {
+    const operationsSection = document.getElementById("operationsSection");
+    operationsSection.innerHTML = "";
+
+    if (selectedStructure === "linkedlist") {
+        const inputField = document.createElement("input");
+        inputField.placeholder = "Element to append";
+        const addButton = document.createElement("button");
+        addButton.innerText = "Append";
+        addButton.onclick = function () {
+            const element = inputField.value;
+            linkedListInstance.append(element);
+            displayOperationResult(`Nodo creado con el elemento: ${element}`);
+            displayLinkedList();
+        };
+
+        operationsSection.appendChild(inputField);
+        operationsSection.appendChild(addButton);
+    }
+    // Otras estructuras de datos
+
+    // Función para mostrar el resultado de la operación
+    function displayOperationResult(result) {
+        const operationResult = document.getElementById("operationResult");
+        operationResult.innerText = result;
+    }
+
+    // Función para mostrar la lista enlazada
+    function displayLinkedList() {
+        const linkedListDisplay = document.createElement("div");
+        linkedListDisplay.innerHTML = "<h2>Lista Enlazada:</h2>";
+        
+        let current = linkedListInstance.head;
+        while (current !== null) {
+            const nodeElement = document.createElement("div");
+            nodeElement.innerText = `Nodo: ${current.element}`;
+            linkedListDisplay.appendChild(nodeElement);
+            current = current.next;
+        }
+
+        operationsSection.appendChild(linkedListDisplay);
     }
 }
